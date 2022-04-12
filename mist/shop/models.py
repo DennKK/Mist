4c9bbc650c0e7d00dae4f63b4c5c4f61a5d7427d
DennKK <87906647+DennKK@users.noreply.github.com>
2022-04-13 00:03:53 +0400

    Update models.py

--- mist/shop/models.py
+++ mist/shop/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.urls import reverse
-from django.utils.text import slugify
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=30)
+    description = models.TextField(default='')
 
     def __str__(self):
         return self.name
@@ -12,12 +11,26 @@ class Genre(models.Model):
 
 class Developer(models.Model):
     name = models.CharField(max_length=50)
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField('')
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
+    category = models.ManyToManyField(Category)
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField()
@@ -25,6 +38,9 @@ class Product(models.Model):
     developer = models.ForeignKey(Developer, on_delete=models.CASCADE)
     release_date = models.DateField()
     price = models.PositiveIntegerField()
+    # poster
+    # screenshots
+    # videos
 
     def __str__(self):
         return self.name
