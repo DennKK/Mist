fd053716490b0095a4e00578380e1863cf9a18aa
DennKK <87906647+DennKK@users.noreply.github.com>
2022-04-13 00:04:37 +0400

    Update urls.py

--- mist/shop/urls.py
+++ mist/shop/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-
 from . import views
-from .views import ProductDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
 ]
