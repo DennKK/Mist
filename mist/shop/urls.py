7805c39a04facbffbf41d2569100c9e21909ed5a
DennKK <karpovdenis.al@gmail.com>
2022-04-13 21:37:48 +0400

    urls fix

--- mist/shop/urls.py
+++ mist/shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import ProductListView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', ProductListView.as_view()),
 ]
