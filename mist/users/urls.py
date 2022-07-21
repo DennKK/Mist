1b2e96f86c258ef0b9514a28c3c425ecd2e42309
DennKK <karpovdenis.al@gmail.com>
2022-07-21 18:08:33 +0400

    small fixes

--- mist/users/urls.py
+++ mist/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import UserLoginView
+from .views import UserLoginView, CreateUserView
 
 urlpatterns = [
-    path('register/', views.register_page, name='register_page'),
+    path('register/', views.CreateUserView.as_view(), name='register_page'),
     path('login/', UserLoginView.as_view(), name='login_page'),
 ]
