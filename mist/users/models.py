9aea6c08ca5c47e6f790230cbade1317506f56b8
DennKK <karpovdenis.al@gmail.com>
2022-07-25 20:54:22 +0400

    small fixes

--- mist/users/models.py
+++ mist/users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(default="images/profile_photo/default_img.png", upload_to="images/profile_photo")
+    photo = models.ImageField(default="images/default_photos/profile_img.png", upload_to="images/profile_photo")
     bio = models.CharField(max_length=100)
 
     def __str__(self):
