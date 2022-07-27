d34e6454905cc56a7757c2daad03a7010d9301e7
DennKK <karpovdenis.al@gmail.com>
2022-07-27 21:14:50 +0400

    added temporary review system

--- mist/shop/views.py
+++ mist/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, UserProductRelationship
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, View
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic.edit import FormView
@@ -14,6 +14,46 @@ class ProductListView(ListView):
     template_name = 'shop/product_list.html'
 
 
+# TEMPORARY
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, url=slug)
+        try:
+            review = request.user.user_product_relationship.get(product=product)
+            form = ReviewForm(
+                instance=review
+            )
+        except:
+            form = ReviewForm()
+
+        context = {
+            "product": product,
+            "form": form,
+        }
+        return render(request, "shop/product_detail.html", context)
+
+    def post(self, request, slug):
+        product=get_object_or_404(Product, url=slug)
+        try:
+            review = request.user.user_product_relationship.get(product=product)
+            form = ReviewForm(
+                request.POST,
+                instance=review
+            )
+            if form.is_valid():
+                form.save()
+            return redirect("shop:products")
+        except:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                new_review = form.save(commit=False)
+                new_review.product = product
+                new_review.user = request.user
+                new_review.save()
+                return redirect("shop:products")
+
+
+'''
 class ProductDetailView(DetailView):
     model =  Product
     slug_field = 'url'
@@ -23,7 +63,8 @@ class ProductDetailView(DetailView):
         context = super(ProductDetailView, self).get_context_data(**kwargs)
         context['user_product_relations'] = UserProductRelationship.objects.all()
         return context
+'''
+
 
 class IndexView(TemplateView):
     template_name = 'shop/index.html'
-
