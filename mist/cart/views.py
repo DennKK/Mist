9f5896ec73a9f9b6a9f6283c72421bb1c9e60f22
DennKK <karpovdenis.al@gmail.com>
2022-08-03 14:50:40 +0400

    added cart views

--- mist/cart/views.py
+++ mist/cart/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart
+from .forms import CartAddProductForm
+from shop.models import Product
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+
+@require_POST
+def cart_add(request, slug):
+    cart = Cart(request)
+    product = get_object_or_404(Product, url=slug)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            product=product,
+            quantity=cd["quantity"],
+            update_quantity=cd["update"]
+        )
+        return redirect("cart:cart_detail")
+
+
+def cart_remove(request, slug):
+    product = get_object_or_404(Product, ulr=slug)
+    cart = Cart(request.POST)
+    cart.remove(product)
+    return redirect("cart:cart_detail")
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, "cart/cart_detail.html", context={"cart": cart})
